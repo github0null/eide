25fccbdf666df0dd8dea367760f77716987f4912
null <me@github0null.io>
2022-09-06 23:22:37 +0800

    support exec script after cmsis pack installed

--- src/PackageManager.ts
+++ src/PackageManager.ts
@@ -22,6 +22,7 @@
 	SOFTWARE.
 */
 
+import * as child_process from 'child_process';
 import * as events from 'events';
 import { File } from '../lib/node-utility/File';
 import { DeleteDir } from './Platform';
@@ -1154,33 +1155,26 @@ export class PackageManager {
             reporter(undefined, 'Start parsing package description file ...');
         }
 
-        // delete list
-        const delList: File[] = outDir.GetAll(File.EMPTY_FILTER, [
-            /^Debug$/i, /^BSP$/i, /^Boards$/i, /^Flash$/i, /^document/i, /^Images$/i
-        ]);
-
-        // delete unused folders
-        for (const dDir of delList) {
-            if (dDir.IsExist()) {
-                console.log('[delete dir] : ' + dDir.name + ' : ' + DeleteDir(dDir));
+        const postCmdFile = File.fromArray([this.project.getEideDir().path, 'post-install.cmsis-pack.sh']);
+        if (postCmdFile.IsFile()) {
+            try {
+                let fpath = outDir.ToRelativePath(postCmdFile.path);
+                fpath = fpath ? `./${fpath}` : File.ToUnixPath(postCmdFile.path);
+                if (fpath.includes(' ')) fpath = `"${fpath}"`;
+                const logTxt = child_process.execSync(`bash ${fpath}`, { cwd: outDir.path }).toString();
+                GlobalEvent.emit('globalLog.append', `\n>>> exec script: '${postCmdFile.name}'\n\n`);
+                GlobalEvent.emit('globalLog.append', logTxt + '\n');
+            } catch (error) {
+                GlobalEvent.emit('msg', newMessage('Warning', `Exec '${postCmdFile.name}' failed !, [path]: ${postCmdFile.path}`));
+                GlobalEvent.emit('msg', ExceptionToMessage(error, 'Hidden'));
             }
         }
-
-        // delete unused files
-        outDir.GetAll([/\.md$/i, /\.chm$/i, /\.pdf$/i, /\.exe$/i, /\.html$/i, /\.jpg$/i, /\.jpeg$/i, /\.png$/i, /\.js$/i, /\.css$/i], File.EMPTY_FILTER)
-            .forEach((f) => {
-                try {
-                    fs.unlinkSync(f.path);
-                } catch (error) {
-                    GlobalEvent.emit('msg', ExceptionToMessage(error, 'Hidden'));
-                }
-            });
 
         try {
             this.LoadPackage(outDir);
         } catch (error) {
-            GlobalEvent.emit('error', error);
             GlobalEvent.emit('msg', newMessage('Warning', `Install package error !, ${(<Error>error).message}`));
+            GlobalEvent.emit('msg', ExceptionToMessage(error, 'Hidden'));
         }
     }
 }
