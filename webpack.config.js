eba12457cd0937f362a9addf64f3816c2e605dd4
Gyan <inaku.gs@gmail.com>
2026-05-05 08:20:09 +0800

    chore(build): 修复 config、deps、ignore 配置问题，优化打包

--- webpack.config.js
+++ webpack.config.js
@@ -4,58 +4,54 @@
 
 const path = require('path');
 
-const config = {
-    target: 'node',
-    entry: './src/extension.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'extension.js',
-        libraryTarget: 'commonjs2',
-        devtoolModuleFilenameTemplate: '../[resource-path]'
-    },
-    devtool: 'source-map',
-    externals: {
-        vscode: 'commonjs vscode',
-        x2js: 'x2js',
-        iconv_lite: 'iconv-lite',
-        jsonc: 'jsonc',
-        ini: 'ini',
-        yaml: 'yaml',
-        unzipper: 'unzipper',
-        jsonc_parser: 'jsonc-parser'
-    },
-    resolve: {
-        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                include: [
-                    path.resolve(__dirname, 'node_modules/jsonc-parser/lib')
-                ],
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [
-                            //'@babel/plugin-proposal-object-rest-spread',
-                            '@babel/plugin-proposal-nullish-coalescing-operator'
-                        ],
+/** @type {(env: any, argv: { mode?: string }) => import('webpack').Configuration} */
+module.exports = (env, argv) => {
+    const isProduction = argv && argv.mode === 'production';
+
+    return {
+        target: 'node',
+        entry: './src/extension.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'extension.js',
+            libraryTarget: 'commonjs2',
+            devtoolModuleFilenameTemplate: '../[resource-path]'
+        },
+        devtool: isProduction ? false : 'source-map',
+        externals: {
+            vscode: 'commonjs vscode'
+        },
+        resolve: {
+            // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    include: [
+                        path.resolve(__dirname, 'node_modules/jsonc-parser/lib')
+                    ],
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                '@babel/plugin-proposal-nullish-coalescing-operator'
+                            ],
+                        },
                     },
                 },
-            },
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
-            }
-        ]
-    }
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'ts-loader'
+                        }
+                    ]
+                }
+            ]
+        }
+    };
 };
-module.exports = config;
